094778467b6be02ba5d9ef878e856a8b543a2f50
Billy Rose <theebillyrose@gmail.com>
2019-12-24 08:27:57 -0700

    trying to stay on the right branch

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,5 +1,7 @@
 {
-   $('#album-title').text(album.title);
-   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('#album-title').text(album.title);
+  $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('.artis').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
 
  }
