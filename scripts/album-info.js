9f4747d0f3d5c787d32e8835f7d8cf3526818be5
Billy Rose <theebillyrose@gmail.com>
2019-12-24 16:13:19 -0700

    fixing very important typo

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,7 +1,7 @@
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
-  $('.artis').text(album.artist);
+  $('.artist').text(album.artist);
   $('#release-info').text(album.releaseInfo);
 
  }
