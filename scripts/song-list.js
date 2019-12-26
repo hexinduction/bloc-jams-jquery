31ca631fb11bdd17de78b8e5801f5b53c34e7083
Billy Rose <theebillyrose@gmail.com>
2019-12-25 17:27:53 -0700

    new song-list updates

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -3,6 +3,13 @@
     song.element = $(`
       <tr>
         <td>${index + 1}</td>
+        <td>
+           <button>
+             <span class="song-number">${index + 1}</span>
+             <span class="ion-play"></span>
+             <span class="ion-pause"></span>
+           </button>
+         </td>
         <td>${song.title}</td>
         <td>${song.duration}</td>
        </tr>
